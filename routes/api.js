9aba1d61bfcd30f0c41d4e84f0db781dcb35f877
AngelCooper <74269067+AngelCooper@users.noreply.github.com>
2021-03-03 20:21:46 -0600

    Update api.js

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const db = require("../models/workout");
+
+//last exercise
+router.get("/api/workouts", async (req, res) => {
+    db.Workout.findOne({})
+    .sort({ day: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
